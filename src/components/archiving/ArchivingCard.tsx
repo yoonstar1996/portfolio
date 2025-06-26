712c37f3cce68b08017a0503bb8d56a80fbd8710
Yoonstar <yagobo1110@naver.com>
2025-06-26 21:57:41 +0900

    Refactor: Image 태그로 변경

--- src/components/archiving/ArchivingCard.tsx
+++ src/components/archiving/ArchivingCard.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 interface ArchivingCardProps {
   imgSrc: string;
+
   alt: string;
+
   label: string;
+
   desc: string;
 }
 
@@ -13,14 +16,22 @@ function ArchivingCard({ imgSrc, alt, label, desc }: ArchivingCardProps) {
     <Link
       href={label}
       target="_blank"
-      className="group w-full max-w-[26rem] break-words rounded-2xl bg-[#f5f5f5] p-[2rem] transition-transform duration-200 ease-linear hover:scale-105 hover:cursor-pointer"
+      className="group w-full max-w-[26rem] break-words rounded-2xl bg-[#f5f5f5] p-[2rem] transition-transform duration-200 ease-linear hover:scale-105 hover:cursor-pointer active:scale-100"
     >
       <div className="mb-4">
-        <img src={imgSrc} alt={alt} className="h-auto w-full" />
+        <Image
+          src={imgSrc}
+          width={352}
+          height={77}
+          alt={alt}
+          className="h-auto w-full"
+        />
       </div>
+
       <div className="mb-4 text-blue-500 group-hover:underline group-hover:decoration-blue-500">
         {label}
       </div>
+
       <div className="text-sm font-semibold text-[#444]">{desc}</div>
     </Link>
   );
